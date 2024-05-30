fa40a520b216d73422a1527ec4563057aeab79dd
CristhianGiron <cristhian.giron@unl.edu.ec>
2024-05-30 13:19:40 -0500

    ultima actualización

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import MainContent1 from "./components/MainContent1";
 import MainContent2 from "./components/MainContent2";
 import DefaultContent from "./components/MainContent3.jsx";
 import MainContent3 from "./components/MainContent4";
+import { Alert, Typography } from "@material-tailwind/react";
+import { CubeTransparentIcon } from "@heroicons/react/24/outline";
 
 
 
@@ -45,6 +47,30 @@ function App() {
           </main>
         </div>
       </div>
+      <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
+          <CubeTransparentIcon className="mb-4 h-12 w-12" />
+          <Typography variant="h6" className="mb-1">
+            Upgrade to PRO
+          </Typography>
+          <Typography variant="small" className="font-normal opacity-80">
+            Upgrade to Material Tailwind PRO and get even more components, plugins, advanced features
+            and premium.
+          </Typography>
+          <div className="mt-4 flex gap-3">
+            <Typography
+              as="a"
+              href="#"
+              variant="small"
+              className="font-medium opacity-80"
+              onClick={() => setOpenAlert(false)}
+            >
+              Dismiss
+            </Typography>
+            <Typography as="a" href="#" variant="small" className="font-medium">
+              Upgrade Now
+            </Typography>
+          </div>
+        </Alert>
     </Router>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = ({ SendOpen, indice }) => {
 
   const [open, setOpen] = React.useState(0);
   const [openAlert, setOpenAlert] = React.useState(true);
- 
+
   const handleOpen = (value) => {
     setOpen(open === value ? 0 : value);
   };
@@ -65,30 +65,6 @@ const Sidebar = ({ SendOpen, indice }) => {
           <PhoneIcon className='min-w-6 h-6' />
           {isOpen && <span className='ml-3 min-w-60'>Contact</span>}
         </div>
-        <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
-        <CubeTransparentIcon className="mb-4 h-12 w-12" />
-        <Typography variant="h6" className="mb-1">
-          Upgrade to PRO
-        </Typography>
-        <Typography variant="small" className="font-normal opacity-80">
-          Upgrade to Material Tailwind PRO and get even more components, plugins, advanced features
-          and premium.
-        </Typography>
-        <div className="mt-4 flex gap-3">
-          <Typography
-            as="a"
-            href="#"
-            variant="small"
-            className="font-medium opacity-80"
-            onClick={() => setOpenAlert(false)}
-          >
-            Dismiss
-          </Typography>
-          <Typography as="a" href="#" variant="small" className="font-medium">
-            Upgrade Now
-          </Typography>
-        </div>
-      </Alert>
       </nav>
     </div>
   );
