b19a2af819af0cd8e028798ca31048836014df86
CristhianGiron <cristhian.giron@unl.edu.ec>
2024-05-30 09:51:07 -0500

    Correciones nuevas nombres preliminares

--- src/components/MainContent2.jsx
+++ src/components/MainContent2.jsx
@@ -4,7 +4,7 @@ import BinaryCalculator from './OperationsBinary';
 const MainContent2 = () => (
   <div>
     <h1 className="text-2xl font-bold mb-4">Operations Binary</h1>
-    <p>Your main content goes here...</p>
+    <p>Calculator: Add Binary, Substract Binary, Multiply Binary, Divide Binary </p>
     <BinaryCalculator/>
   </div>
 );
