3fdba4269e1ab244909af0110f31f045936341b7
CristhianGiron <cristhian.giron@unl.edu.ec>
2024-05-30 10:51:31 -0500

    Alert to pro

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,8 +7,9 @@ import {
   XMarkIcon,
   PuzzlePieceIcon,
   PhoneIcon,
+  CubeTransparentIcon,
 } from "@heroicons/react/24/outline";
-import { IconButton } from '@material-tailwind/react';
+import { Alert, IconButton, Typography } from '@material-tailwind/react';
 
 const Sidebar = ({ SendOpen, indice }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,6 +25,13 @@ const Sidebar = ({ SendOpen, indice }) => {
     indice(index);
   };
 
+
+  const [open, setOpen] = React.useState(0);
+  const [openAlert, setOpenAlert] = React.useState(true);
+ 
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
   return (
     <div className={`flex flex-col absolute z-[1000] ${isOpen ? 'lg:w-64 w-96' : 'w-16'} h-full bg-gray-800 text-white transition-width duration-700`}>
       <div className="flex items-center justify-between h-16 px-4 bg-gray-900">
@@ -57,6 +65,30 @@ const Sidebar = ({ SendOpen, indice }) => {
           <PhoneIcon className='min-w-6 h-6' />
           {isOpen && <span className='ml-3 min-w-60'>Contact</span>}
         </div>
+        <Alert open={openAlert} className="mt-auto" onClose={() => setOpenAlert(false)}>
+        <CubeTransparentIcon className="mb-4 h-12 w-12" />
+        <Typography variant="h6" className="mb-1">
+          Upgrade to PRO
+        </Typography>
+        <Typography variant="small" className="font-normal opacity-80">
+          Upgrade to Material Tailwind PRO and get even more components, plugins, advanced features
+          and premium.
+        </Typography>
+        <div className="mt-4 flex gap-3">
+          <Typography
+            as="a"
+            href="#"
+            variant="small"
+            className="font-medium opacity-80"
+            onClick={() => setOpenAlert(false)}
+          >
+            Dismiss
+          </Typography>
+          <Typography as="a" href="#" variant="small" className="font-medium">
+            Upgrade Now
+          </Typography>
+        </div>
+      </Alert>
       </nav>
     </div>
   );
